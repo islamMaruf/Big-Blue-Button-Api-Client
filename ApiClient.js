9d319448f6e12f656b28b88bbe10015d97a6a0c5
islamMaruf <marufislam7424@gmail.com>
2023-02-15 17:55:45 +0600

    file added

---
+++ ApiClient.js
@@ -0,0 +1,103 @@
+const axios = require('axios');
+const querystring = require('querystring')
+const crypto = require('hash.js')
+const parser = require('fast-xml-parser')
+const uuid = require('uuid');
+
+
+class BigBlueButtonApiClient {
+    async makeApiCall(action, params = {}, type = 'GET') {
+        try {
+            const response = await this.httpClient(action, params, type)
+            return response;
+        } catch (error) {
+            console.log('error',error.message)
+            return error;
+        }
+    }
+
+    httpClient(action, params, type) {
+        let URL = this.constructUrl(action, params)
+        return axios(URL, {
+            method: type,
+            headers: {Accept: 'text/xml, application/json, text/plain, */*'},
+        }).then((response) => {
+            return response.data
+        }).then((xml) => {
+            return this.parseXml(xml)
+        })
+    }
+
+    constructUrl(action, params) {
+        const BIG_BLUE_BUTTON_API_URL = process.env.BIGBLUEBUTTON_API_URL;
+        const BIG_BLUE_BUTTON_SECRET = process.env.BIGBLUEBUTTON_SECRET
+        params.checksum = this.generateChecksum(action, params, BIG_BLUE_BUTTON_SECRET)
+        const URL = `${BIG_BLUE_BUTTON_API_URL}/api/${action}?${querystring.encode(params)}`
+        console.log('URL',URL)
+        return URL;
+    }
+
+    generateChecksum(callName, queryParams, secret) {
+        return crypto
+            .sha1()
+            .update(`${callName}${querystring.encode(queryParams)}${secret}`)
+            .digest('hex')
+    }
+
+    parseXml(xml) {
+        const jsonFormat = parser.parse(xml).response
+
+        if (jsonFormat?.meetings) {
+            let meetings = jsonFormat.meetings ? jsonFormat.meetings.meeting : []
+            meetings = Array.isArray(meetings) ? meetings : [meetings]
+            jsonFormat.meetings = meetings
+        }
+        if (jsonFormat?.recordings) {
+            let recordings = jsonFormat.recordings ? jsonFormat.recordings.recording : []
+            recordings = Array.isArray(recordings) ? recordings : [recordings]
+            jsonFormat.recordings = recordings
+        }
+        return jsonFormat
+    }
+
+    async createMeeting(params) {
+        params.meetingID = uuid.v4()
+        console.log('create meeting params', params)
+        return this.makeApiCall('create', params,'POST');
+    }
+
+    async joinMeeting(params) {
+        params.redirect = 'false'
+        console.log('join meeting params', params)
+        return this.makeApiCall('join', params);
+    }
+
+    async endMeeting(params){
+        return this.makeApiCall('end')
+    }
+
+    async getMeetingInfo(params) {
+        return this.makeApiCall('getMeetingInfo', params);
+    }
+
+    async getMeetings() {
+        return this.makeApiCall('getMeetings');
+    }
+
+    async createWebhook(params) {
+        return this.makeApiCall('hooks/create', params)
+    }
+
+    async getWebhooks(params) {
+        return this.makeApiCall('hooks/list', params)
+    }
+
+    async destroyHooks(params) {
+        return this.makeApiCall('hooks/destroy', params)
+    }
+
+    // Add more API methods as needed...
+}
+
+const apiClient = new BigBlueButtonApiClient();
+module.exports = apiClient;
---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+require('dotenv').config()
+const apiClient = require('./ApiClient')
+const fs = require('fs');
+const path = require('path')
+
+app.use(morgan('tiny'))
+app.use(morgan('dev',{
+    stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+}));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.json({greetings: 'Hello world'});
+});
+
+app.post('/webhook', (req, res) => {
+    const {event, payload} = req.body;
+    fs.appendFile("output.json", JSON.stringify(req.body), 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+
+        console.log("JSON file has been saved.");
+    });
+
+    switch (event) {
+        case 'meeting-created':
+            console.log(`Meeting created: ${JSON.stringify(payload)}`);
+            break;
+        case 'meeting-ended':
+            console.log(`Meeting ended: ${JSON.stringify(payload)}`);
+            break;
+        case 'user-joined':
+            console.log(`User joined the meeting: ${JSON.stringify(payload)}`);
+            break;
+        case 'user_left':
+            console.log(`User left the meeting: ${JSON.stringify(payload)}`);
+            break;
+        case 'participant_joined_voice':
+            console.log(`Participant joined voice: ${JSON.stringify(payload)}`);
+            break;
+        case 'participant_left_voice':
+            console.log(`Participant left voice: ${JSON.stringify(payload)}`);
+            break;
+        case 'participant_sharing_video':
+            console.log(`Participant sharing video: ${JSON.stringify(payload)}`);
+            break;
+        case 'participant_stopped_sharing_video':
+            console.log(`Participant stopped sharing video: ${JSON.stringify(payload)}`);
+            break;
+        case 'participant_raised_hand':
+            console.log(`Participant raised hand: ${JSON.stringify(payload)}`);
+            break;
+        case 'participant_lowered_hand':
+            console.log(`Participant lowered hand: ${JSON.stringify(payload)}`);
+            break;
+        default:
+            console.log(`Unknown event received: ${event}`);
+    }
+
+    res.sendStatus(200);
+});
+
+app.post('/create-meeting', async (req, res) => {
+    const data = req.body
+    const response = await apiClient.createMeeting(data)
+    res.json(response)
+});
+
+app.post('/join-meeting', async (req, res) => {
+    const data = req.body;
+    const response = await apiClient.joinMeeting(data)
+    res.json(response)
+
+})
+
+app.get('/meetings', async (req, res) => {
+    const response = await apiClient.getMeetings()
+    res.json(response)
+})
+
+app.get('/meeting/:meetingId', async (req, res) => {
+    console.log('body', req.body)
+    // const response = await apiClient.getMeetingInfo()
+})
+
+app.get('/webhooks', async (req, res) => {
+    const response = await apiClient.getWebhooks()
+    res.json(response)
+})
+
+app.post('/create-webhook', async (req, res) => {
+    const data = req.body;
+    console.log(data)
+    const response = await apiClient.createWebhook(data)
+    res.json(response)
+})
+
+app.post('/delete-webhook', async (req, res) => {
+    const data = req.body;
+    const response = await apiClient.destroyHooks(data)
+    res.json(response)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
